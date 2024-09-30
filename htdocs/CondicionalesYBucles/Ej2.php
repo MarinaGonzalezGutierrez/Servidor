944840a333979ba19b3c5b92f48390cbddc33ad0
Marina González <marinagogu4@gmail.com>
2024-09-30 14:03:49 +0200

    Coreccion del ej 2

--- htdocs/CondicionalesYBucles/Ej2.php
+++ htdocs/CondicionalesYBucles/Ej2.php
@@ -4,20 +4,17 @@ tres. -->
 <?php
 $num1 = 4;
 $num2 = 7;
-$num3 = 2;
+$num3 = 10;
 
-if($num1 > $num2){
-    if($num1 >  $num3){
-        echo "El numero mayor es el :".$num1;
-    }
-}else if($num2 > $num1){
-    if($num2 > $num3){
-        echo "El numero mayor es el :".$num2;
-    }
+if($num1 > $num2 && $num1 >  $num3){
+     echo "El numero mayor es el :".$num1;
     
-}else if($num3 > $num2){
-    if($num3 > $num1){
+}else if($num2 > $num1 && $num2 > $num3){
+    echo "El numero mayor es el :".$num2;
+
+    
+}else if($num3 > $num2 && $num3 > $num1){
         echo "El numero mayor es el :".$num3;
-    }
+    
 }
 ?>
