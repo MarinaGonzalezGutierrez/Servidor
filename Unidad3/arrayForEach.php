84142a6aa58fa65739a7c9ab283fed78131cafe9
Marina González <marinagogu4@gmail.com>
2024-10-02 13:41:12 +0200

    Unidad 3 primer ejemplo

---
+++ Unidad3/arrayForEach.php
@@ -0,0 +1,9 @@
+<!-- asignar nombres a animales -->
+<?php
+ $mascotas = array('perro' => "Yunito",
+ 'gato' => "Wilson",
+ 'canario' => "Piolin",
+ 'tortuga' => "Pepe");
+ foreach($mascotas as $item => $description)
+ echo "$item: $description<br>";
+?> 
