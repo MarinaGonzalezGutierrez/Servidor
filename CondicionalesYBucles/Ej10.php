cf6b7367831ffffe1f92ce12ab4169072ff8fa07
Marina González <marinagogu4@gmail.com>
2024-10-03 10:13:47 +0200

    Otro

--- CondicionalesYBucles/Ej10.php
+++ CondicionalesYBucles/Ej10.php
@@ -1,13 +1,15 @@
+<!-- Generar todos los pares de números formados por combinaciones de dígitos del 1
+al 9, siendo además los dos componentes del par distintos. -->
+
 <?php
 echo "Pares de números con dígitos del 1 al 9:\n";
 
-// Recorrer todos los dígitos del 1 al 9 para el primer número
 for ($i = 1; $i <= 9; $i++) {
-    // Recorrer todos los dígitos del 1 al 9 para el segundo número
+
     for ($j = 1; $j <= 9; $j++) {
-        // Asegurarse de que los números del par sean distintos
+
         if ($i != $j) {
-            echo "($i, $j)\n"; // Imprimir el par
+            echo "($i, $j)\n"; 
         }
     }
 }
