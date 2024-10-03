fe4314e531bf12cf89ef049dc29d5a2651e27cff
Marina González <marinagogu4@gmail.com>
2024-10-03 10:11:07 +0200

    Cambiar

--- CondicionalesYBucles/Ej3.php
+++ CondicionalesYBucles/Ej3.php
@@ -6,24 +6,20 @@ doble de una hora normal cuando no exceden de 8; si las horas extras exceden de
 al triple. -->
 
 <?php
-//Asignar valores a variables
 $horas_trabajadas = 50; 
 $pago_por_hora = 10;
 
-// Inicializar la variable del pago total
 $pago_total = 0;
 
 if($horas_trabajadas > 40){
-    // Calcular horas extras
+   
     $horas_extras = $horas_trabajadas-40;
     $horas_normales = 40;
 
     // Si las horas extras son menores o iguales a 8
     if($horas_extras <= 8){
-        // Pago por horas normales + pago por horas extras (al doble)
         $pago_total = ($horas_normales*$pago_por_hora)+($horas_extras*$pago_por_hora*2);
     }else{ 
-        // Si las horas extras exceden de 8
         $horas_extras_dobles = 8;
         $horas_extras_triples = $horas_extras- $horas_extras_dobles;
  
@@ -35,12 +31,11 @@ if($horas_trabajadas > 40){
 
 
 }else{
-    // Si no hay horas extras, solo paga las horas trabajadas normalmente
+
     $pago_total = $horas_trabajadas * $pago_por_hora;
 
 }
 
-// Mostrar el resultado del pago total
 echo "El pago total por las horas trabajadas es: €" . $pago_total;
 
 ?>
