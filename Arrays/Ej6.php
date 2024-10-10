ed87b61f81738ed24ff2b0d9dff2220796b8edc5
Marina González <marinagogu4@gmail.com>
2024-10-10 12:23:55 +0200

    ej6

--- Arrays/Ej6.php
+++ Arrays/Ej6.php
@@ -3,9 +3,13 @@ fila y columna del elemento mayor -->
 
 <?php
 $matrizCompleta = array();
+$mayor =  -1; //numero menor de los que se hagan automaticamente
+$filaMayor = 0;
+$columnaMayor = 0;
+
 for($i=0; $i<4; $i++){
     for($j=0; $j<5; $j++){
-        $matrizCompleta[$i][$j] = rand(1, 10);
+        $matrizCompleta[$i][$j] = rand(1, 50);
 
     }
 }
@@ -16,6 +20,18 @@ for($i=0; $i<4; $i++){
     }
     echo "<br/>";
 }
+
+//para buscar el numero mayor
+for($i=0; $i< 4; $i++){
+    for($j=0; $j<5; $j++){
+        if($matrizCompleta[$i][$j] > $mayor){
+            $mayor = $matrizCompleta[$i][$j];
+            $filaMayor = $i;
+            $columnaMayor = $j;
+        }
+    }
+}
+echo "El numero mayor se encuentra en la fila ".$filaMayor. " y columna ".$columnaMayor;
 
 
 
--- FuncionesYObjetos/ecuacionSegundoGrado.php
+++ FuncionesYObjetos/ecuacionSegundoGrado.php
@@ -3,29 +3,33 @@ los coeficientes de la ecuación y devuelve un array con las soluciones. Si no h
 soluciones reales, devuelve FALSE. -->
 
 <?php
-function ecuacion_Segundo_Grado($a, $b, $c, $discr, $solucion){
+function ecuacion_Segundo_Grado($a, $b, $c, $discr, $solucion[]){
 $a = 3;
 $b = 2;
 $c = 1;
 
-$discriminante = ($b*$b) - (4 * $a* $c);
+$discr = ($b*$b) - (4 * $a* $c);
 
-if($discriminante < 0){
+if($discr < 0){
     echo "La ecuación no tiene soluciones reales (discriminante negativo).";
     return $solucion = false;
 }
 
-elseif($discriminante  == 0){
+elseif($discr  == 0){
 // Una única solución: -b / 2a
 $solucion = -$b / (2*$a);
 echo "La ecuación tiene una única solución: $solucion";
 $solucion = true;
 }else{
     // Dos soluciones reales distintas
-    $solucion1 = (-$b + sqrt($discriminante)) / (2 * $a);
-    $solucion2 = (-$b - sqrt($discriminante)) / (2 * $a);
- $solucion = true;
+    $solucion = (-$b + sqrt($discr)) / (2 * $a);
+    $solucion = (-$b - sqrt($discr)) / (2 * $a);
+    echo "Solución 1: $solucion <br>";
+    echo "Solución 2: $solucion";
 
+ $solucion = true;
+for($i = 0; $i < 2; $i++){
+    echo $solucion[$i]; 
 }
 }
 
