e693831272e38ae4bbe04650b850e2e4873bfa72
Marina González <marinagogu4@gmail.com>
2024-10-10 13:36:32 +0200

    ejemplo objetos

---
+++ Arrays/Ej8.php
@@ -0,0 +1,2 @@
+<!-- 8. Hacer un algoritmo que llene una matriz de 10x10 con valores aleatorios y
+determine la posición [fila, columna] del número mayor almacenado en la matriz. -->
---
+++ Objetos/Persona/persona.php
@@ -0,0 +1,16 @@
+<?php
+class Persona {
+    public $nombre;
+    public $apellido;
+    public $edad;
+
+   
+
+
+
+
+
+}
+
+
+?>
