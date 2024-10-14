4efa16e7d390e69b6de4e412c3af1ba683a44be0
Marina González <marinagogu4@gmail.com>
2024-10-14 14:22:21 +0200

    Ej20

--- Uni3_Pra1/Ej20.php
+++ Uni3_Pra1/Ej20.php
@@ -16,11 +16,25 @@ $estadios_Futbol = array("Barcelona" => "Camp Nou,", "Real Madrid" => "Santiago 
 "Valencia" => "Mestalla", "Real Sociedad" => "Anoeta");
 
 //mostrar valores en una tabla
+echo "Mostrar valores en una tabla";
 echo "<table border='1'>";
 echo "<tr><th>Índice</th><th>Valor</th></tr>";
 foreach ($estadios_Futbol as $indice => $valor) {
     echo "<tr><td>$indice</td><td>$valor</td></tr>";
 }
 echo "</table>";
+
+unset($estadios_Futbol["Real Madrid"]); //unset: para eliminar un elemento del array
+//eliminar real madrid 
+echo "<br/>";
+echo "<br/>";
+echo "Eliminar real madrid: ";
+echo "<table border='1'>";
+echo "<tr><th>Índice</th><th>Valor</th></tr>";
+foreach ($estadios_Futbol as $indice => $valor) {
+    echo "<tr><td>$indice</td><td>$valor</td></tr>";
+}
+echo "</table>";
+
 
 ?>
