2551ae9723f91c69205f8fca5a2958b36a13ac8c
Marina González <marinagogu4@gmail.com>
2024-10-16 13:57:13 +0200

    ej3php

---
+++ Uni3_Pra3/Ej3.php
@@ -0,0 +1,10 @@
+<!-- 3. Escribe una función que reciba una cadena y comprueba si es un palíndromo.-->
+ <?php
+function esPalindromo($cadena){
+    
+}
+
+//cadena escrita
+$cadena = ("Así Mario oirá misa");
+
+?>
