c0d87d69768da1587b07a9eb6f8928a379ea88ff
Marina González <marinagogu4@gmail.com>
2024-10-23 14:21:22 +0200

    apartado mostrar

--- ClasesYObjetos_Prac4/Ej4/mostrar.php
+++ ClasesYObjetos_Prac4/Ej4/mostrar.php
@@ -5,9 +5,8 @@ include ('vehiculo.php');
 /* Aplique el método repintar(color) para cambiar el color definido en la
 clase Vehículo.  */
 
-$vehiculo = new vehiculo("verde", 500);
-$vehiculo -> repintar("verde");
-echo "El color del coche es :".$coche -> set_Color()."<br>";
+$coche = new Coche("rojo", 2000, 4, 0);
+echo $coche;
 
 
 ?>
