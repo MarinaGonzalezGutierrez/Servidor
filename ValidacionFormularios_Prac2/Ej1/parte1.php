4d661338e7cc86390b4c7eecb8d49efdf1b6762c
Marina González <marinagogu4@gmail.com>
2024-11-04 13:14:37 +0100

    parte2 ej1

--- ValidacionFormularios_Prac2/Ej1/parte1.php
+++ ValidacionFormularios_Prac2/Ej1/parte1.php
@@ -3,7 +3,9 @@ $valor = " Es tu nombre O\'reilly? ";
 $resultado = trim($valor);
 echo $resultado;
 
-//La linea de codigo (trim($valor)) lo que hace es mostrar $valor, quitando los espacios en blanco que 
-//tiene al principio y al final la frase.
+/*
+La linea de codigo (trim($valor)) lo que hace es mostrar la frase de la variable $valor, quitando
+ los espacios en blanco que tiene al principio y al final la frase.
+*/
 
 ?>
---
+++ ValidacionFormularios_Prac2/Ej1/parte2.php
@@ -0,0 +1,12 @@
+<?php
+$valor = " Es tu nombre O\'reilly? ";
+$resultado = stripslashes($valor);
+echo $resultado;
+
+/*
+La linea de codigo (striplashes($valor)) lo que hace es mostrar la frase de la variable $valor,
+eliminando la barra invertida (\).
+*/
+
+
+?>
