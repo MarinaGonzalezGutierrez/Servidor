d48393b7dbc6b2d61515df23e2f2095382811ec8
Marina González <marinagogu4@gmail.com>
2024-11-04 13:08:33 +0100

    parte1 ej1

--- ValidacionFormularios_Prac2/Ej1/parte1.php
+++ ValidacionFormularios_Prac2/Ej1/parte1.php
@@ -1,6 +1,9 @@
 <?php
+$valor = " Es tu nombre O\'reilly? ";
+$resultado = trim($valor);
+echo $resultado;
 
-
-
+//La linea de codigo (trim($valor)) lo que hace es mostrar $valor, quitando los espacios en blanco que 
+//tiene al principio y al final la frase.
 
 ?>
