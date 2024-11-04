15d695391e922cf7b937acd27e9eee93e535c13c
Marina González <marinagogu4@gmail.com>
2024-11-04 13:34:19 +0100

    ej3

---
+++ ValidacionFormularios_Prac2/Ej3/ej3.php
@@ -0,0 +1,7 @@
+
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+
+<!-- EL metodo post significa que los datos del formulario se enviaran al servidor usando este metodo
+ Action especifica la url a la que se enviaran los datos del formulario. El fragmento que esta en
+ php lo que hace es imprimir la url del script que se esta ejecutando.
+ -->
