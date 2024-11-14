b6a5471e4e85e3757f003e6fdf51905b5f8156e9
Marina González <marinagogu4@gmail.com>
2024-11-14 13:39:28 +0100

    ejercicio 3

--- AutoevaluacionControl/ejercicio3/ejercicio3.php
+++ AutoevaluacionControl/ejercicio3/ejercicio3.php
@@ -1,41 +1,42 @@
-<?php
-//ABRIMOS SESION
-session_start();
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $_SESSION["miNumero"]=rand(1,100);
-    
-    if(isset ($_POST['numero'])){
-        if($_SESSION["miNumero"] === $_POST['numero']){
-            echo "Tu numero es : $numero";
-            echo "ENHORABUENA HAS ACERTADO";
-           
-        }
-    }
-   
-    
-   
-}
-
-
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Juego del numero secreto:</title>
+    <title>Adivinar numero</title>
 </head>
 <body>
-    <form action="#" method="POST">
-    <!-- Caja para introducir el numero -->
-    <label for="numero">Adivina mi numero : </label>
-    <input type="text" id="numero" name ="numero" required min="1"><br><br>
-    <!-- Boton para enviar -->
-    <input type="submit" value="Enviar">
-    </form>
 
+<?php
+session_start(); //Esta línea inicia la sesión en PHP
+
+if (!isset($_SESSION['miNumero'])) { //Aquí se verifica si existe la variable de sesión
+    $_SESSION["miNumero"] = rand(1, 100); 
+    $_SESSION['intentos'] = 0;
+
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_POST['numIntroducir'])) {
+        $numIntroducir = $_POST['numIntroducir'];
+
+        if ($numIntroducir < $_SESSION['miNumero']) {
+            echo "El numero es mayor <br>";
+            $_SESSION['intentos']++;
+        } elseif ($numIntroducir> $_SESSION['miNumero']) {
+            echo "El numero es menor <br>";
+            $_SESSION['intentos']++;
+        } else {
+            echo "Enhorabuena, has acertado el numero en " . $_SESSION['intentos'] . " intentos";
+            session_destroy(); // Cierra la sesión
+        }
+    }
+}
+?>
+
+<label>Adivina mi numero:</label>
+<form method="POST" action="Ej3.php"> 
+    <input type="text" name="numIntroducir">
+    <input type="submit" value="Enviar">
+</form>
 </body>
 </html>
