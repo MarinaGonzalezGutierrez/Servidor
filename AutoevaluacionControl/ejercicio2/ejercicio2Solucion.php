6047aa6176b1bd19b99e7d20a20a0cd264f8ee47
Marina González <marinagogu4@gmail.com>
2024-11-18 14:25:08 +0100

    ejrcicio cookies

---
+++ AutoevaluacionControl/ejercicio2/ejercicio2Solucion.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+$_SESSION["resul"] = 0;  
+
+
+
+
+?>
---
+++ Cookies/Ej1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies seleccionar colores</title>
+</head>
+<body>
+    <h2>Selecciona ede que color  </h2>
+<form action="#" method="POST">
+        <input type="radio" name="opcion" value="1">Menos de 14 años
+        
+        <br>
+        
+        <input type="radio" name="opcion" value="2" >Entre 15 y 20 años
+        
+        <br>
+        
+   
+</form>
+</body>
+</html>
