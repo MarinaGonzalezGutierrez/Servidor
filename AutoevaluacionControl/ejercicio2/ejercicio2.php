1564a9a0d4be4d79293afeb595f774d8a591624c
Marina González <marinagogu4@gmail.com>
2024-11-19 09:51:15 +0100

    ej resuelto

--- AutoevaluacionControl/ejercicio2/ejercicio2.php
+++ AutoevaluacionControl/ejercicio2/ejercicio2.php
@@ -1,35 +1,56 @@
+<?php
+session_start();
+$_SESSION["resul"] = 0;
+$bin = [rand(0,1),rand(0,1),rand(0,1),rand(0,1)];
+$potencias = [8,4,2,1];
+ 
+for ($i=0; $i<4; $i++) {
+    if($bin[$i] == 1)  {
+        $_SESSION["resul"] = $_SESSION["resul"] + $potencias[$i];
+    }
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adivina el numero en decimal :</title>
-
-    <style>
-.cuadrado {
-  width: 120px;           
-  height: 150px;       
-  background: black;       
-  border: 1px solid #000; 
-}
-
-.cuadrado1{
-width: 120px;           
-  height: 150px;       
-  background: white;       
-  border: 1px solid #000; 
-}
-
-
-    </style>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
 </head>
 <body>
-    <h2> Adivina el numero en decimal </h2>
-    <h5>El numero en binario es:<?php $num = rand(1,100); echo $num; ?>  </h5>
-
-    <div class="cuadrado">
-
-</div>
-
+<h1>Adivina el numero decimal</h1>
+<h2>El numero en BINARIO: <?php foreach($bin as $num){echo $num;}?></h2>
+<?php
+        if($bin[0] == 1) {
+            echo "<img src='img/ocho.jpg'>";
+        } else {
+            echo "<img src='img/blanco.jpg'>";
+        }
+ 
+        if($bin[1] == 1) {
+            echo "<img src='img/cuatro.jpg'>";
+        } else {
+            echo "<img src='img/blanco.jpg'>";
+        }
+ 
+        if($bin[2] == 1) {
+            echo "<img src='img/dos.jpg'>";
+        } else {
+            echo "<img src='img/blanco.jpg'>";
+        }
+ 
+        if($bin[3] == 1) {
+            echo "<img src='img/uno.jpg'>";
+        } else {
+            echo "<img src='img/blanco.jpg'>";
+        }
+ 
+    ?>
+<br><br>
+<form action="ejercicio21.php" method="post">
+<label for="resp">Número decimal</label>
+<input type="number" name="resp" required>
+<input type='submit' value='Enviar'>
+</form>
 </body>
 </html>
--- AutoevaluacionControl/ejercicio2/ejercicio2Solucion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION["resul"] = 0;  
-
-
-
-
-?>
