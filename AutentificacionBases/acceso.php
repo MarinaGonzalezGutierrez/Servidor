5c99c6ea410b30539a938567acf283023fdd3293
Marina González <marinagogu4@gmail.com>
2024-11-20 14:00:37 +0100

    primera autentificacion

---
+++ AutentificacionBases/acceso.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comprobar usuario y contraseña</title>
+</head>
+<body>
+<form action="prueba.php" method="post">
+        <label for="usuario">Usuario: </label>
+        <input type="text" id="usuario" name ="usuario" required min="1"><br><br>
+
+        <label for="contrasena">Contraseña: </label>
+        <input type="password" id="contrasena" name="contrasena" required min="1"><br><br>
+
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
---
+++ AutentificacionBases/prueba.php
@@ -0,0 +1,16 @@
+<?php
+$usu = 'pepito';
+$contra = '123';
+
+
+
+if($usu == $_POST['usuario'] && $contra == $_POST['contrasena']){
+    echo "Bienvenidoo!!";
+
+}else{
+    echo "Acceso denegado!!";
+}
+
+
+
+?>
