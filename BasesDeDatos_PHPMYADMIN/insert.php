ad3cef95f2d4ad550252843b44a84263f92ea4d0
Marina González <marinagogu4@gmail.com>
2024-11-27 13:34:42 +0100

    insert

---
+++ BasesDeDatos_PHPMYADMIN/insert.php
@@ -0,0 +1,14 @@
+<?php
+ require_once 'login.php';
+
+ $connection = new mysqli($hn, $un, $pw, $db, 3307); //puerto
+ if ($connection->connect_error) die("Fatal Error");
+
+
+ $consulta="INSERT INTO usuarios (Usu, Contra, Rol) VALUES ('Yolanda', 'yolanda123', 'jugador')";
+
+
+ $result = $connection->query($consulta);
+mysqli_close($connection);
+
+?>
