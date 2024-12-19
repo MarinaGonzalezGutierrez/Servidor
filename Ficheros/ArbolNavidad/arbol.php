66ddd1c32b4677e7f89d685c8e2fb95d3140879c
Marina González <marinagogu4@gmail.com>
2024-12-19 13:26:47 +0100

    ests subido

--- Ficheros/ArbolNavidad/arbol.php
+++ Ficheros/ArbolNavidad/arbol.php
@@ -1,11 +1,75 @@
 <?php
-$directorio = "uploads/";
-$rutaArchivo = $directorio . basename($_FILES["archivoSubir"]["name"]);
-$tipoExtension= pathinfo($rutaArchivo,PATHINFO_EXTENSION);
-$comprobar = getimagesize($_FILES["archivoSubir"]["tmp_name"]);
+$target_dir = "subidas/";  // Directorio donde se guardarán los archivos subidos
+$target_file = $target_dir. basename($_FILES["Imagen0"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen0"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen0"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
 
+    $target_file = $target_dir. basename($_FILES["Imagen1"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen1"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen1"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
 
+    $target_file = $target_dir. basename($_FILES["Imagen2"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen2"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen2"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
 
+    $target_file = $target_dir. basename($_FILES["Imagen3"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen3"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen3"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
+
+    $target_file = $target_dir. basename($_FILES["Imagen4"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen4"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen4"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
+
+    $target_file = $target_dir. basename($_FILES["Imagen5"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen5"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen5"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
+
+    $target_file = $target_dir. basename($_FILES["Imagen6"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen6"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen6"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
+
+    $target_file = $target_dir. basename($_FILES["Imagen7"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen7"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen7"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
+
+    $target_file = $target_dir. basename($_FILES["Imagen8"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen8"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen8"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
+
+    $target_file = $target_dir. basename($_FILES["Imagen9"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["Imagen9"]["tmp_name"], $target_file)) {
+        echo "El archivo " .basename($_FILES["Imagen9"]["name"]) . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
 
 
 ?>
+
--- Ficheros/TareaAutodidactaEjercicio/upload.php
+++ Ficheros/TareaAutodidactaEjercicio/upload.php
@@ -1,39 +1,10 @@
 <?php
-//en q fichero esta
-//check = comprobar
-$directorio = "uploads/";
-//ruta completa de donde esta
-$rutaArchivo = $directorio . basename($_FILES["archivoSubir"]["name"]);
-$booleano = 1; //variable booleana q comprueba q el fichero se subio
-//tipo de extension: txt, jpg, docx
-$tipoExtension= pathinfo($rutaArchivo,PATHINFO_EXTENSION);
-// Comprobamos si es una imagen real o falsa
- $comprobar = getimagesize($_FILES["archivoSubir"]["tmp_name"]);
- if($comprobar !== false) {
- echo "El fichero es una imagen - " . $comprobar["mime"] . ".";
- $booleano = 1;
- } else {
- echo "El fichero no es una imagen.";
- //aqui cuando el fichero no se subio y explica el porque
- $booleano = 0;
- }
-// }
-// Comprobamos si el fichero ya existe
-if (file_exists($rutaArchivo)) {
-    echo "El fichero existe.";
-    $booleano = 0;
-   }
+$target_dir = "subidas/";  // Directorio donde se guardarán los archivos subidos
+$target_file = $target_dir. basename($_FILES["archivoSubir"]["name"]);  // Añadimos un prefijo único al nombre del archivo para evitar sobreescribirlo
+    if (move_uploaded_file($_FILES["archivoSubir"]["tmp_name"], $target_file)) {
+        echo "El archivo " .$target_file . " ha sido subido correctamente.";
+    } else {
+        echo "Hubo un error al mover el archivo al directorio de destino.";
+    }
 
-   // comprobar el tamaño del archivo
-   //300*1024
-if ($_FILES["archivoSubir"]["size"] > 307200) {
-    echo "El fichero es demasiado grande.";
-    $booleano = 0;
-   }
-
-   // solo aceptar este formato
-if($tipoExtension != "txt" ) {
- echo "Lo siento, solo el tipo txt esta permitido.";
- $booleano = 0;
-}
 ?>
