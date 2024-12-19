dd38267482f46fe44146a9d48fed2ea95e8e39e6
Marina González <marinagogu4@gmail.com>
2024-12-19 13:52:13 +0100

    acabar

--- Ficheros/ArbolNavidad/arbol.php
+++ Ficheros/ArbolNavidad/arbol.php
@@ -73,3 +73,94 @@ $target_file = $target_dir. basename($_FILES["Imagen0"]["name"]);  // Añadimos 
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arbol de Navidad</title>
+</head>
+
+<style>
+ body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f8ff;
+            margin: 0;
+        }
+
+        .arbolNavidad {
+            text-align: center;
+        }
+
+        .level {
+            display: flex;
+            justify-content: center;
+            margin-bottom: -10px; 
+        }
+
+        .level img {
+            width: 100px; 
+            height: auto;
+            margin: 0 5px; 
+        }
+
+        .primera {
+            margin-top: 0; 
+        }
+
+        .segunda {
+            margin-top: 0;
+        }
+
+        .tercera {
+            margin-top: 0;
+        }
+
+        .cuarta {
+            margin-top: 0;
+        }
+
+        .tronco {
+            margin-top: -10px; 
+        }
+
+        .tronco img {
+            width: 40px; 
+            height: auto;
+        }
+ </style>
+<body>
+<div class="arbolNavidad">
+        <div class="primera level">
+            <img src="subidas/1.jpg" />
+        </div>
+        
+        <div class="segunda level">
+            <img src="subidas/1.jpg" />
+            <img src="subidas/1.jpg" />
+        </div>
+
+        <div class="tercera level">
+            <img src="subidas/1.jpg" />
+            <img src="subidas/1.jpg" />
+            <img src="subidas/1.jpg" />
+        </div>
+
+        <div class="cuarta level">
+            <img src="subidas/1.jpg" />
+            <img src="subidas/1.jpg" />
+            <img src="subidas/1.jpg" />
+            <img src="subidas/1.jpg" />
+        </div>
+
+        <div class="tronco">
+            <img src="subidas/1.jpg" />
+        </div>
+    </div>
+ 
+</body>
+</html>
+
